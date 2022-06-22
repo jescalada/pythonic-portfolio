63a614e709ecc0f0ae5530aba7fef0cbd0ecba47
Juan Escalada <juanescalada175@gmail.com>
2022-06-22 10:52:48 -0700

    Add post timeline tester script

---
+++ endpoint_tester.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:5000/api/timeline_post -d 'name=Testing&email=testing@test.com&content=Just a test. Hello World!'
+
+curl -X GET http://localhost:5000/api/timeline_post
