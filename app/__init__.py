af4ad69c9ac76c15be6278f7231b46060b5420fd
Juan Escalada <juanescalada175@gmail.com>
2022-06-24 11:47:01 -0700

    Add simple post viewing frontend:

--- app/__init__.py
+++ app/__init__.py
@@ -57,7 +57,11 @@ def profile(name):
 
 @app.route('/timeline')
 def timeline():
-    return render_template('timeline.html', title='Timeline')
+    posts = [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+            ]
+    return render_template('timeline.html', title='Timeline', posts=posts)
 
 
 @app.route('/api/timeline_post', methods=['POST'])
